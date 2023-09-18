409ffaf563b4e5be2eba12945d870ad9fbe70972
ltuhn <80274740+ltuhn@users.noreply.github.com>
2023-09-18 21:51:01 +0200

    Add files via upload

---
+++ Java/Oblig 6/Aapning.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class Aapning extends HvitRute {
+    public Aapning(Labyrint labyrint, int rad, int kolonne) {
+        super(labyrint, rad, kolonne);
+    }
+}
---
+++ Java/Oblig 6/SortRute.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class SortRute extends Rute {
+
+    public SortRute(Labyrint labyrint, int rad, int kolonne) {
+        super(labyrint, rad, kolonne);
+    }
+
+    @Override
+    public String charTilTegn() {
+        return "#";
+    }
+
+    @Override
+    public void gaa(Rute rute, ArrayList<Tuppel> vei) {
+        return;
+    }
+
+}

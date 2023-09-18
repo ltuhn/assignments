c37fa4ad2be8fc5620496c6d4424d29801ab875f
ltuhn <80274740+ltuhn@users.noreply.github.com>
2023-09-18 21:39:27 +0200

    Delete oblig4 directory

--- oblig4/Liste.java
+++
@@ -1,9 +0,0 @@
-interface Liste<T> extends Iterable<T> {
-    public int stoerrelse();
-    public void leggTil(int pos, T x);
-    public void leggTil(T x); // sette inn element paa slutten av listen
-    public void sett(int pos, T x); // sette inn elementet paa angitt posisjon + overskrive det som var der fra for av
-    public T hent(int pos); // hente ut elem paa oppgitt indeks
-    public T fjern(int pos); // fjerne element paa gitt indeks i listen. etterfolgende elems faar en lavere posisjon
-    public T fjern(); // fjerne og returnere elementet paa starten av listen
-}
--- oblig4/Vanlig.java
+++
@@ -1,7 +0,0 @@
-public class Vanlig extends Legemiddel {
-    
-    public Vanlig(String navn, int pris, double vStoff) {
-        super(navn, pris, vStoff); //samme som superklassen
-    }
-    
-}

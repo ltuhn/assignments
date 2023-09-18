f7aef81d141078d4d140c546d560562362da94c1
ltuhn <80274740+ltuhn@users.noreply.github.com>
2023-09-18 21:51:30 +0200

    Delete Java/Oblig 6 directory

--- Java/Oblig 6/Aapning.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-
-public class Aapning extends HvitRute {
-    public Aapning(Labyrint labyrint, int rad, int kolonne) {
-        super(labyrint, rad, kolonne);
-    }
-}
